731cbf8fe8477ef156697cb112028e6fe608304a
John Borges <jab24@njit.edu>
2012-06-29 18:13:27 -0400

    added AJAX form login functionality

--- public/jqtouch/mobile.js
+++ public/jqtouch/mobile.js
@@ -3,7 +3,8 @@ var jQT = new $.jQTouch({
   //addGlossToIcon: false,
   startupScreen:"apple-touch-startup.png",
   statusBar:'black-translucent',
-  preloadImages:[
+  formSelector:'.form',
+	preloadImages:[
     '/jqtouch/themes/jqt/img/back_button.png',
     '/jqtouch/themes/jqt/img/back_button_clicked.png',
     '/jqtouch/themes/jqt/img/button_clicked.png',
@@ -33,4 +34,29 @@ jQuery(function() {
 	//$("#home").bind("pageAnimationEnd" ,function(e,info){
 	//	alert("Animating #home " + info.direction);
 	//});
+	$("#login a.login").tap(function(e){
+		var $form = $(this).closest("form");
+		return app.login($form);
+	});
+	$("#login").submit(function(e){
+		var $form = $(this);
+		return app.login($form);
+	});
 });
+var app = {
+  login:function ($form) {
+    $.ajax({
+      type:$form.attr("method"), url:$form.attr("action"),
+      dataType:"json", data:$form.serialize(),
+      complete:function (req) {
+        if (req.status === 200 || req.status === 304) {
+          jQT.goBack();
+        } else {
+          alert("There was an error logging in. Try again.");
+        }
+      }
+    });    
+    
+    return false;
+  }
+};
