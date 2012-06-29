ca7c71e6001ff3a2496a79888e1d33b4fb3fa25b
John Borges <jab24@njit.edu>
2012-06-29 17:57:01 -0400

    added AJAX functionality to load month data

--- public/jqtouch/mobile.js
+++ public/jqtouch/mobile.js
@@ -16,9 +16,21 @@ var jQT = new $.jQTouch({
 jQuery(function() {
   // Add custom handler code here.
 	$("#month").bind("pageAnimationEnd" ,function(e,info){
+		if(info.direction == "out")return;
+		var $page = $(this);
+		if($page.data("loaded"))return;
+		$.getJSON("/month.json",function(){
+			var $li = $("month ul:first li:first");
+			JQuery.each(data,function(){
+				var $clone = $li.clone();
+				$clone.html(this.title + "<small>" + this.quantity + "</small>");
+				$li.parent().append($clone);
+			});
+			$page.data("loaded", true);
+		});
 		alert("Animating #month " + info.direction);
 	});
-	$("#home").bind("pageAnimationEnd" ,function(e,info){
-		alert("Animating #home " + info.direction);
-	});
+	//$("#home").bind("pageAnimationEnd" ,function(e,info){
+	//	alert("Animating #home " + info.direction);
+	//});
 });
